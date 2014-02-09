ebb774c134d83a3b1f0130345ba10794ce935ecf
guimaraes13 <victorlopes13@gmail.com>
2014-02-09 12:57:47 -0200

    IOUtil new functions

--- learning-with-dl/dl-learner-utilities/src/main/java/edu/uff/dllearnerUtil/cliUtil/IOUtil.java
+++ learning-with-dl/dl-learner-utilities/src/main/java/edu/uff/dllearnerUtil/cliUtil/IOUtil.java
@@ -3,13 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.uff.dllearnerUtil.cliUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 import org.apache.commons.io.IOUtils;
 
 /**
@@ -17,6 +20,7 @@ import org.apache.commons.io.IOUtils;
  * @author Victor
  */
 public class IOUtil {
+
     public static final String SUFFIX = ".tmp";
 
     public static File stringToFile(String fileContext, String virtualFileName) throws IOException {
@@ -27,5 +31,28 @@ public class IOUtil {
         IOUtils.copy(in, out);
         in.close();
         return tempFile;
+    }
+
+    public static String readFile(String filePath) throws FileNotFoundException, IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line = null;
+        
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+    
+    public static String readFile(String[] filePath) throws FileNotFoundException, IOException {
+        StringBuilder sb = new StringBuilder();
+        for (String fp : filePath) {
+            sb.append(readFile(fp));
+            sb.append("\n");
+        }
+        
+        return sb.toString();
     }
 }
